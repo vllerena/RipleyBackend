36ba810f778a983965a12bf16a591e56479fcf80
vllerena <victor_llerena@outlook.com>
2021-09-25 14:11:17 -0500

    #2 Instalacion y configuracion de MySQL Express Connection

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mysql = require('mysql');
+const myconn = require('express-myconnection');
 
 const app = express();
-
 app.set('port', process.env.PORT || 9090)
+const config = {
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'ripleyBackend'
+}
+
+app.use(myconn(mysql, config, 'single'))
 
 app.get('/', (req, res) => {
     res.send('Bienvenido a Ripley API')
